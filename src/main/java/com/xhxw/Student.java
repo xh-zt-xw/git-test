1946bf2d4d19962a8f6ecb73d5015fac0b87a644
xh-zt-xw <70921901+xh-zt-xw@users.noreply.github.com>
2023-05-17 16:10:23 +0800

    Update Student.java
    远程修改

--- src/main/java/com/xhxw/Student.java
+++ src/main/java/com/xhxw/Student.java
@@ -4,11 +4,12 @@ package com.xhxw;
  * @author Zalmant
  */
 public class Student {
-
+    
     private String name;
     private String password;
     private String gender;
     private int age;
     private String address;
     private String telephone;
+    private String stunum;
 }
